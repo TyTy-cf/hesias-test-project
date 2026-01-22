bfa323216ae82a30eb15ef67415f62e98b32a94c
Kévin Tourret <kevin@drosalys.fr>
2026-01-22 10:48:24 +0100

    test pipeline ?

--- src/test/java/fr/kevin/cap_enterprise/tests/BaseTest.java
+++ src/test/java/fr/kevin/cap_enterprise/tests/BaseTest.java
@@ -8,15 +8,27 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 abstract public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeEach
-    public void setDriver() {
+    protected void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        String headless = System.getenv("HEADLESS");
+        if ("true".equalsIgnoreCase(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 

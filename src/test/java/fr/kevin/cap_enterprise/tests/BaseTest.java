90c8e6007ea0ebfad10776b3803ea4bb875c3fa6
Kévin Tourret <kevin@drosalys.fr>
2026-01-22 10:11:50 +0100

    add allure

--- src/test/java/fr/kevin/cap_enterprise/tests/BaseTest.java
+++ src/test/java/fr/kevin/cap_enterprise/tests/BaseTest.java
@@ -1,8 +1,11 @@
 package fr.kevin.cap_enterprise.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,8 +23,18 @@ abstract public class BaseTest {
     @AfterEach
     public void quit() {
         if (driver != null) {
-//            driver.quit();
+            try {
+                saveScreenshot();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            driver.quit();
         }
+    }
+
+    @Attachment(fileExtension = ".png", type = "image/png", value = "Screenshot of the test")
+    public byte[] saveScreenshot() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
 }
--- src/test/java/fr/kevin/cap_enterprise/tests/HomeTest.java
+++ src/test/java/fr/kevin/cap_enterprise/tests/HomeTest.java
@@ -1,6 +1,7 @@
 package fr.kevin.cap_enterprise.tests;
 
 import fr.kevin.cap_enterprise.pages.HomePage;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,12 +9,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class HomeTest extends BaseTest {
 
     @Test
+    @Feature("Searchbar")
+    @Story("As the user, I want to search 'Cybepunk' on the website")
+    @Description("Click on the search button, then type 'Cyberpunk' to search for it")
+    @Severity(SeverityLevel.NORMAL)
     public void testSearchSuccess() {
         String currentUrl = new HomePage(driver) // return HomePage
                 .searchItem("Cyberpunk") // return SearchResultPage
                 .getCurrentUrl(); // return the currentUrl (String)
 
-        assertTrue(currentUrl.contains("/search?term=Cyberpunk"));
+        System.out.println(currentUrl);
+        assertTrue(currentUrl.contains("term=Cyberpunk"));
+    }
+
+
+    @Test
+    @Feature("Searchbar")
+    @Story("As the user, I want to search 'Arc raider' on the website")
+    @Description("Click on the search button, then type 'Arc raider' to search for it")
+    @Severity(SeverityLevel.NORMAL)
+    public void testSearchFailed() {
+        String currentUrl = new HomePage(driver) // return HomePage
+                .searchItem("Arc raider") // return SearchResultPage
+                .getCurrentUrl(); // return the currentUrl (String)
+
+        System.out.println(currentUrl);
+        assertTrue(currentUrl.contains("term=Cyberpunk"));
     }
 
 }
